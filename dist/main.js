bdd35b303e8c2241f3ac59a057c0adda13e5911b
Queenshiba <haruka0111.2a@gmail.com>
2022-10-31 18:47:36 +0900

    add trigger to jigyonaiyo

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+let hiddenMenuDiv = document.getElementById("hidden-menu")
+function jigyonaiyoMenuTrigger() {
+
+
+    if (hiddenMenuDiv.style.display === "block") {
+        hiddenMenuDiv.style.display = "none"
+
+    } else {
+        hiddenMenuDiv.style.display = "block"
+
+    }
+    
+
+}
+
