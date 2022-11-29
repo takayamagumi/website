1b092f9b79ca5d557fdaf25499beb58c1d7efcbd
Queenshiba <haruka0111.2a@gmail.com>
2022-11-29 15:58:08 +0900

    fixed mobile

--- dist/main.js
+++ dist/main.js
@@ -5,8 +5,6 @@ function jigyonaiyoMenuTrigger() {
   } else {
     hiddenMenuDiv.style.display = "block"
   }
-
-
 }
 
 let hiddenMenuDivMobile = document.getElementById("hidden-menu-mobile")
@@ -16,8 +14,6 @@ function jigyonaiyoMenuTriggerMobile(){
   } else {
     hiddenMenuDivMobile.style.display = "block"
   }
-
-
 }
 
 
@@ -29,6 +25,8 @@ let navIcon = document.getElementById('nav-icon')
 let navIconOpen = document.getElementById('nav-icon-open')
 let navIconClosed = document.getElementById('nav-icon-closed')
 let mobileMenuContainer = document.getElementById('mobile-menu-container')
+
+
 
 navIcon.addEventListener('click', ()=>
   navIcon.classList.toggle('open')
@@ -46,4 +44,36 @@ navIcon.addEventListener('click', function(){
 });
 
 
+let menuIconsMobile = document.getElementsByClassName('menu-icons-mobile')
+let mobileMenuLink = document.getElementsByClassName('mobile-menu-link')
 
+for (let i = 0; i < mobileMenuLink.length; i++) {
+  mobileMenuLink[i].addEventListener('click', function(){
+  // alert('test')
+  if(menuIconsMobile.className === 'flex') {
+    menuIconsMobile.style.display = 'none'
+}else {
+  menuIconsMobile.style.display = 'flex'
+}
+});
+}
+
+// Get the button:
+let goToTopBtn = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goToTopBtn.style.display = "block";
+  } else {
+    goToTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
