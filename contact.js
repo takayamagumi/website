9c87994892ff0f90105216617cc33360be2181db
Queenshiba <haruka0111.2a@gmail.com>
2023-01-14 16:45:22 +0900

    Updates

---
+++ contact.js
@@ -0,0 +1,13 @@
+let contactBtnToconfirm = document.getElementById('contact-btn-toconfirm')
+
+function goToConfirm() {
+
+    let companyNamevalue = document.getElementById('companyName').value
+    let companyNameFuriganavalue = document.getElementById('companyNameFurigana').value
+    let namevalue = document.getElementById('name').value
+    let nameFuriganavalue = document.getElementById('nameFurigana').value
+    let emailAdressvalue = document.getElementById('emailAdress').value
+    let phoneNumbervalue = document.getElementById('phoneNumber').value
+    let contentTextvalue = document.getElementById('content-text').value
+    alert(companyNamevalue)
+}
