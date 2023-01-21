1ff32cd17335f3461831f4cc4567ddb2b617a61a
Queenshiba <haruka0111.2a@gmail.com>
2023-01-22 00:15:56 +0900

    add arr for checked label

--- dist/contact.js
+++ dist/contact.js
@@ -2,6 +2,19 @@ let contactBtnToconfirm = document.getElementById('contact-btn-toconfirm')
 
 function goToConfirm() {
 
+    let labels = document.getElementsByClassName('labelsForPurpose');
+    let inputs = document.getElementsByClassName('checkboxes-purpose'),
+        selectedCheckboxes = [];
+    console.log(inputs.length)
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].type === "checkbox" && inputs[i].checked) {
+            selectedCheckboxes.push(labels[i].textContent);
+            // selectedCheckboxes.push(inputs[i].value);
+        }
+    }
+
+    let arr = []
+    arr.push(selectedCheckboxes)
     let companyNamevalue = document.getElementById('companyName').value
     let companyNameFuriganavalue = document.getElementById('companyNameFurigana').value
     let namevalue = document.getElementById('name').value
@@ -9,7 +22,13 @@ function goToConfirm() {
     let emailAdressvalue = document.getElementById('emailAdress').value
     let phoneNumbervalue = document.getElementById('phoneNumber').value
     let contentTextvalue = document.getElementById('content-text').value
-    let arr = []
+
     arr.push(companyNamevalue, companyNameFuriganavalue, namevalue, nameFuriganavalue, emailAdressvalue, phoneNumbervalue, contentTextvalue)
     console.log(arr)
+
+
+
+    return arr;
+
+
 }
