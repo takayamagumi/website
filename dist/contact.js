eb4983bed5285a13aae8138c5a99c8424616c130
Queenshiba <haruka0111.2a@gmail.com>
2023-02-06 22:51:28 +0900

    checkboxes err

--- dist/contact.js
+++ dist/contact.js
@@ -2,11 +2,12 @@
 let contactBtnToconfirm = document.getElementById('contact-btn-tosend')
 
 function goToConfirm() {
-
+    // alert('tes')
+    // console.log('tes')
     let labels = document.getElementsByClassName('labelsForPurpose');
     let inputs = document.getElementsByClassName('checkboxes-purpose'),
         selectedCheckboxes = [];
-    console.log(inputs.length)
+    // console.log(inputs.length)
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].type === "checkbox" && inputs[i].checked) {
             selectedCheckboxes.push(labels[i].textContent);
@@ -14,34 +15,36 @@ function goToConfirm() {
         }
     }
 
-    let arr = []
-    arr.push(selectedCheckboxes)
-    let companyNamevalue = document.getElementById('companyName').value
-    let companyNameFuriganavalue = document.getElementById('companyNameFurigana').value
-    let namevalue = document.getElementById('name').value
-    let nameFuriganavalue = document.getElementById('nameFurigana').value
-    let emailAdressvalue = document.getElementById('emailAdress').value
-    let phoneNumbervalue = document.getElementById('phoneNumber').value
-    let contentTextvalue = document.getElementById('content-text').value
+    // let arr = []
+    // arr.push(selectedCheckboxes)
+    // let companyNamevalue = document.getElementById('companyName').value
+    // let companyNameFuriganavalue = document.getElementById('companyNameFurigana').value
+    // let namevalue = document.getElementById('name').value
+    // let nameFuriganavalue = document.getElementById('nameFurigana').value
+    // let emailAdressvalue = document.getElementById('emailAdress').value
+    // let phoneNumbervalue = document.getElementById('phoneNumber').value
+    // let contentTextvalue = document.getElementById('content-text').value
 
-    arr.push(companyNamevalue, companyNameFuriganavalue, namevalue, nameFuriganavalue, emailAdressvalue, phoneNumbervalue, contentTextvalue)
-    // console.log(arr)
-    let labelsArr = ["お問い合わせ内容", "会社名", "会社名(フリガナ)", "お名前", "お名前(フリガナ)", "メールアドレス", "電話番号", "お問い合わせ内容"]
+    // arr.push(companyNamevalue, companyNameFuriganavalue, namevalue, nameFuriganavalue, emailAdressvalue, phoneNumbervalue, contentTextvalue)
+    // // console.log(arr)
+    // let labelsArr = ["お問い合わせ内容", "会社名", "会社名(フリガナ)", "お名前", "お名前(フリガナ)", "メールアドレス", "電話番号", "お問い合わせ内容"]
     let test = []
-    for (let i = 0; i < labelsArr.length; i++) {
-        let item = []
-        item.push(labelsArr[i])
-        item.push(arr[i])
-        test.push(item)
+    // for (let i = 0; i < labelsArr.length; i++) {
+    //     let item = []
+    //     item.push(labelsArr[i])
+    //     item.push(arr[i])
+    //     test.push(item)
+    // }
+
+    let checkboxesPurpose = document.getElementsByClassName('checkboxes-purpose');
+    for (let c = 0; c < checkboxesPurpose.length; c++) {
+        if (checkboxesPurpose[c].checked) {
+            // document.getElementById('contact-checkbox-policy').style.backgroundColor = "red";
+            console.log('yes1',checkboxesPurpose[c].name)
+        } 
     }
 
-    let checkboxPolicy = document.getElementsByClassName('checkbox-policy');
-    if (checkboxPolicy.checked) {
-        // document.getElementById('contact-checkbox-policy').style.backgroundColor = "red";
-        alert("checked")
-    }
 
-    // console.log(test)
     return test;
 }
 
