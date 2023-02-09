46bcf835ff100b6cef33e3e9eb462ba41a4a7d2c
Queenshiba <haruka0111.2a@gmail.com>
2023-02-09 14:54:08 +0900

    Updates

--- contact.js
+++ contact.js
@@ -1,18 +1,21 @@
 
 let contactBtnToconfirm = document.getElementById('contact-btn-tosend')
 
+contactBtnToconfirm.addEventListener('click', function () {
 
+    goToConfirm()
 
-
+}, false);
 
 
 
 function goToConfirm() {
-
+    // alert('tes')
+    // console.log('tes')
     let labels = document.getElementsByClassName('labelsForPurpose');
     let inputs = document.getElementsByClassName('checkboxes-purpose'),
         selectedCheckboxes = [];
-    console.log(inputs.length)
+    // console.log(inputs.length)
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].type === "checkbox" && inputs[i].checked) {
             selectedCheckboxes.push(labels[i].textContent);
@@ -20,28 +23,98 @@ function goToConfirm() {
         }
     }
 
-    let arr = []
-    arr.push(selectedCheckboxes)
-    let companyNamevalue = document.getElementById('companyName').value
-    let companyNameFuriganavalue = document.getElementById('companyNameFurigana').value
+    // let arr = []
+    // arr.push(selectedCheckboxes)
+    // let companyNamevalue = document.getElementById('companyName').value
+    // let companyNameFuriganavalue = document.getElementById('companyNameFurigana').value
+    // arr.push(companyNamevalue, companyNameFuriganavalue, namevalue, nameFuriganavalue, emailAdressvalue, phoneNumbervalue, contentTextvalue)
+    // // console.log(arr)
+    // let labelsArr = ["お問い合わせ内容", "会社名", "会社名(フリガナ)", "お名前", "お名前(フリガナ)", "メールアドレス", "電話番号", "お問い合わせ内容"]
+    // let test = []
+    // for (let i = 0; i < labelsArr.length; i++) {
+    //     let item = []
+    //     item.push(labelsArr[i])
+    //     item.push(arr[i])
+    //     test.push(item)
+    // }
+
+    // error
+    let errorContainer = document.getElementById('error-container')
+    errorContainer.innerHTML = '';
+    // let contactFormInputWrap = document.getElementById('contact-form-input-wrap')
+    // contactFormInputWrap.innerHTML = '';
+
+    //  checkboxes of purpose 
+    let contactCheckboxWrapPurpose = document.querySelector('#contact-checkbox-wrap-purpose');
+
+    let checked = contactCheckboxWrapPurpose.querySelectorAll('input[type="checkbox"]:checked')
+
+    if (checked.length >= 1) {
+        // console.log('yes', checked)
+    } else {
+        let errorContainer = document.getElementById('error-container')
+        errorContainer.style.display = "";
+        let checkboxesPurposeErrorMessage = document.createElement('p')
+        let checkboxesPurposeErrorMessageText = document.createTextNode('●お問い合わせ種別は最低１つ選択してください。')
+        checkboxesPurposeErrorMessage.appendChild(checkboxesPurposeErrorMessageText)
+        errorContainer.appendChild(checkboxesPurposeErrorMessage)
+        document.getElementById('recruit').style.outline = "solid 2px red";
+        document.getElementById('work').style.outline = "solid 2px red";
+        document.getElementById('other').style.outline = "solid 2px red";
+    }
+
+    // requred input if not filled
+    let requredLabelsArr = ["お名前", "お名前(フリガナ)", "メールアドレス", "電話番号", "お問い合わせ内容"]
+
+    let nameInput = document.getElementById('name')
+    let nameFuriganaInput = document.getElementById('nameFurigana')
+    let emailAdressInput = document.getElementById('emailAdress')
+    let phoneNumberInput = document.getElementById('phoneNumber')
+    let contentTextInput = document.getElementById('content-text')
+
+    let requredInput = [nameInput, nameFuriganaInput, emailAdressInput, phoneNumberInput, contentTextInput]
+
     let namevalue = document.getElementById('name').value
     let nameFuriganavalue = document.getElementById('nameFurigana').value
     let emailAdressvalue = document.getElementById('emailAdress').value
     let phoneNumbervalue = document.getElementById('phoneNumber').value
     let contentTextvalue = document.getElementById('content-text').value
 
-    arr.push(companyNamevalue, companyNameFuriganavalue, namevalue, nameFuriganavalue, emailAdressvalue, phoneNumbervalue, contentTextvalue)
-    // console.log(arr)
-let labelsArr = ["お問い合わせ内容","会社名","会社名(フリガナ)","お名前","お名前(フリガナ)","メールアドレス","電話番号","お問い合わせ内容"]
-let test = []
-for (let i = 0; i < labelsArr.length; i++) {
-    let item = []
-    item.push(labelsArr[i])
-    item.push(arr[i])
-test.push(item)
-}
+    let requredInputArr = [namevalue, nameFuriganavalue, emailAdressvalue, phoneNumbervalue, contentTextvalue]
+    for (let r = 0; r < requredInputArr.length; r++) {
+        let arr = []
+        if (requredInputArr[r] === '') {
+            console.log(requredInputArr[r])
+            arr.push(requredLabelsArr[r])
+            arr.push(requredInputArr[r])
+            console.log(arr)
 
-console.log(test)
-    return test;
+            let errorContainer = document.getElementById('error-container')
+            errorContainer.style.display = "";
+            let checkboxesRequredErrorMessage = document.createElement('p')
+            let checkboxesRequredErrorMessageText = document.createTextNode('●' + requredLabelsArr[r] + 'を入力してください。')
+            checkboxesRequredErrorMessage.appendChild(checkboxesRequredErrorMessageText)
+            errorContainer.appendChild(checkboxesRequredErrorMessage)
+
+            requredInput[r].style.outline = "solid 2px red";
+
+        }
+
+    }
+
+
+    // checkbox privacy policy
+    let checkboxPolicy = document.getElementById('checkbox-policy');
+    if (!checkboxPolicy.checked) {
+        let errorContainer = document.getElementById('error-container')
+        errorContainer.style.display = "";
+        let checkboxesPrivacyPolicyMessage = document.createElement('p')
+        let checkboxesPrivacyPolicyMessageText = document.createTextNode('●プライバシーポリシーの同意にチェックをしてください。')
+        checkboxesPrivacyPolicyMessage.appendChild(checkboxesPrivacyPolicyMessageText)
+        errorContainer.appendChild(checkboxesPrivacyPolicyMessage)
+        document.getElementById('checkbox-policy').style.outline = "solid 2px red";
+
+    }
+
 }
 
