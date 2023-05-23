308571735dcb34bca4785a903092a3aaef537c01
Queenshiba <haruka0111.2a@gmail.com>
2023-05-23 15:36:30 +0900

    add tables at work page

--- dist/work.js
+++ dist/work.js
@@ -1,10 +1,143 @@
+
+
+
+
+
 fetch('./work.json')
     .then((response) => response.json())
     .then((json) => {
         let workdata = json
-        console.log(workdata.workdata[0])
-        let workdatayear = workdata.workdata[0]['施工実績年度']
-        let workdatayeardata = workdata.workdata[0]['施工実績データ']
-        console.log(workdatayeardata[0]['工事名'])
-    }
-    )
+        let workdatabyarrays = workdata.workdata
+        console.log(workdatabyarrays)
+        let workdatayear = workdatabyarrays[0]['施工実績年度']
+        let workdatayeardata = workdatabyarrays[0]['施工実績データ']
+
+
+
+        // reiwa 3 nen
+        let workdatabyarraydatar3 = workdatabyarrays[0]['施工実績データ']
+
+        for (let k = 0; k < workdatabyarraydatar3.length; k++) {
+            let workdatabyarraydatatd = workdatabyarraydatar3[k];
+            let kojiDate = workdatabyarraydatatd['完成年月']
+            let kojiName = workdatabyarraydatatd['工事名']
+            let kojiPlace = workdatabyarraydatatd['工事場所']
+            let kojiOrderer = workdatabyarraydatatd['工事発注者']
+            let kojiPict = workdatabyarraydatatd['写真']
+
+            if (kojiPict === "") {
+                kojiPict = "無し"
+            }
+
+            let kojiDatetextnode = document.createTextNode(kojiDate);
+            let kojiNametextnode = document.createTextNode(kojiName);
+            let kojiPlacetextnode = document.createTextNode(kojiPlace);
+            let kojiOrderertextnode = document.createTextNode(kojiOrderer);
+            let kojiPicttextnode = document.createTextNode(kojiPict);
+
+
+            let td;
+            let tr;
+            tr = document.createElement("tr");
+
+            td = document.createElement("td");
+            td.appendChild(kojiOrderertextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            td.appendChild(kojiNametextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            td.appendChild(kojiPlacetextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            td.appendChild(kojiDatetextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            let imglink = document.createElement('a')
+
+            if (kojiPict === "無し") {
+                td.appendChild(kojiPicttextnode)
+            } else {
+                imglink.href = kojiPicttextnode
+                imglink.appendChild(kojiPicttextnode)
+                td.appendChild(imglink)
+            }
+
+            tr.appendChild(td)
+
+            let tableforworkr3 = document.getElementById('tableforworkr3')
+            tableforworkr3.appendChild(tr)
+        }
+
+        // reiwa 3 nen
+        let workdatabyarraydatar2 = workdatabyarrays[1]['施工実績データ']
+
+        for (let k = 0; k < workdatabyarraydatar2.length; k++) {
+            let workdatabyarraydatatd = workdatabyarraydatar2[k];
+            let kojiDate = workdatabyarraydatatd['完成年月']
+            let kojiName = workdatabyarraydatatd['工事名']
+            let kojiPlace = workdatabyarraydatatd['工事場所']
+            let kojiOrderer = workdatabyarraydatatd['工事発注者']
+            let kojiPict = workdatabyarraydatatd['写真']
+
+            if (kojiPict === "") {
+                kojiPict = "無し"
+            }
+
+            let kojiDatetextnode = document.createTextNode(kojiDate);
+            let kojiNametextnode = document.createTextNode(kojiName);
+            let kojiPlacetextnode = document.createTextNode(kojiPlace);
+            let kojiOrderertextnode = document.createTextNode(kojiOrderer);
+            let kojiPicttextnode = document.createTextNode(kojiPict);
+
+
+            let td;
+            let tr;
+            tr = document.createElement("tr");
+
+            td = document.createElement("td");
+            td.appendChild(kojiOrderertextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            td.appendChild(kojiNametextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            td.appendChild(kojiPlacetextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            td.appendChild(kojiDatetextnode)
+            tr.appendChild(td)
+
+            td = document.createElement("td");
+            let imglink = document.createElement('a')
+
+            if (kojiPict === "無し") {
+                td.appendChild(kojiPicttextnode)
+            } else {
+                imglink.href = kojiPicttextnode
+                imglink.appendChild(kojiPicttextnode)
+                td.appendChild(imglink)
+            }
+
+            tr.appendChild(td)
+
+            let tableforworkr2 = document.getElementById('tableforworkr2')
+            tableforworkr2.appendChild(tr)
+        }
+
+
+
+
+
+
+
+    })
+
+
